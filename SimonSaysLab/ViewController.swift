e61f8c5c276d7fe7d35597ec2500b8c039f67e56
JoyceMatos <j.matosjoyce@gmail.com>
2016-09-06 19:28:06 -0400

    Completed

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -10,14 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBAction func red(sender: AnyObject) {
+        buttonsClicked += 1
+        print("hey red is being clicked")
+        simonSaysGame.guessRed()
+        playedGame ()
+
+        }
+    
+    @IBAction func green(sender: AnyObject) {
+        buttonsClicked += 1
+        print("hey green is being clicked")
+        simonSaysGame.guessGreen()
+        playedGame ()
+
+    }
+    
+    @IBAction func yellow(sender: AnyObject) {
+        buttonsClicked += 1
+        print("hey yellow is being clicked")
+        simonSaysGame.guessYellow()
+        playedGame ()
+
+    }
+    
+    @IBAction func blue(sender: AnyObject) {
+        buttonsClicked += 1
+        print("hey blue is being clicked")
+        simonSaysGame.guessBlue()
+        playedGame ()
+
+    }
+    
+    @IBOutlet weak var youWon: UILabel!
+    
+    @IBOutlet weak var tryAgain: UILabel!
+    
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+    
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
+
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.hidden = true
+        youWon.hidden = true
+        tryAgain.hidden = true
+
     }
 }
 
@@ -47,4 +92,15 @@ extension ViewController {
                 }
         })
     }
+    
+    func playedGame () {
+        if buttonsClicked == 5 && simonSaysGame.chosenColors == simonSaysGame.patternToMatch {
+            youWon.hidden = false
+
+        } else if
+            buttonsClicked == 5 && simonSaysGame.chosenColors != simonSaysGame.patternToMatch  {
+            tryAgain.hidden = false
+        }
+    
+}
 }
